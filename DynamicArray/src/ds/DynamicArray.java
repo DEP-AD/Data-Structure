d9583df0d9dd66eb99f6e9463bfb7652dfa6cc40
Aruni Dissanayake <aruni.saku@gmail.com>
2021-05-07 11:57:56 +0530

    implemet methods of dynamicArray

---
+++ DynamicArray/src/ds/DynamicArray.java
@@ -0,0 +1,44 @@
+package ds;
+
+public class DynamicArray {
+
+    private int [] DynamicArray;
+
+    public void add(int a){
+
+    }
+
+    public void add(int a, int b){
+
+    }
+
+    public void remove(int index){
+
+    }
+
+    public int get(int index){
+        return 0;
+    }
+
+    public void print(){
+
+    }
+
+    public void clear(){
+
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public boolean contains(int number){
+        return false;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+
+}
